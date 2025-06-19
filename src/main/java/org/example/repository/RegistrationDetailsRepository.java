95e5796f6c444fae623fab9c966a595a2522b368
Saikiran-deepak <kiransai101144444@gmail.com>
2025-06-19 23:21:58 +0530

    bug fixed

--- src/main/java/org/example/repository/RegistrationDetailsRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.repository;
-
-import org.example.RegistrationDetails;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RegistrationDetailsRepository extends JpaRepository<RegistrationDetails, Long> {
-}
